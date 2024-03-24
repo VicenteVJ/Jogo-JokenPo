ddf9bb9f55dfbb45f0b60caf0a69f7376c856462
Vicente Joel <106132901+VicenteVJ@users.noreply.github.com>
2024-03-24 00:34:39 -0300

    ajustes no css

--- js/main.js
+++ js/main.js
@@ -1,51 +1,27 @@
-function maquina(jogada) {
-    var numero =  Math.floor(Math.random() * 3);
-    if (numero == 0 && jogada == '1') {
-        alert('maquina jogou pedra');
-        alert('empate');
-    }
-    if (numero == 0 && jogada == '2') {
-        alert('maquina jogou pedra');
-        alert('voce ganhou');
-    }
-    if (numero == 0 && jogada == '3') {
-        alert('maquina jogou pedra');
-        alert('voce perdeu');
-    }
-    if (numero == 1 && jogada == '1') {
-        alert('maquina jogou papel');
-        alert('voce perdeu');
-    }
-    if (numero == 1 && jogada == '2') {
-        alert('maquina jogou papel');
-        alert('empate');
-    }
-    if (numero == 1 && jogada == '3') {
-        alert('maquina jogou papel');
-        alert('voce ganhou');
-    }
-    if (numero == 2 && jogada == '1') {
-        alert('maquina jogou tesoura');
-        alert('voce ganhou');
-    }
-    if (numero == 2 && jogada == '2') {
-        alert('maquina jogou tesoura');
-        alert('voce perdeu');
-    }
-    if (numero == 2 && jogada == '3') {
-        alert('maquina jogou tesoura');
-        alert('empate');
-    }
-}
+function makeMove(playerMove) {
+    var moves = ['Pedra', 'Papel', 'Tesoura'];
+    var machineMove = Math.floor(Math.random() * 3) + 1;
+    var resultMessage = '';
 
-alert('Bem vindo ao Pedra, Papel e Tesoura!')
-if (prompt('Aperte 1 para comecar a jogar ') === '1') {
-    alert('Escolha a sua jogada: ');
-    var jgdJogador = prompt(`
-                            1 - Pedra
-                            2 - Papel
-                            3 - Tesoura
-                            `);
-    if (jgdJogador === '1' || jgdJogador === '2' || jgdJogador === '3') 
-    maquina(jgdJogador);
+    var playerChoice = moves[playerMove - 1];
+    var machineChoice = moves[machineMove - 1];
+
+    document.getElementById('player-choice').innerHTML = `<img src="imagens/${playerChoice}.png" alt="${playerChoice}" class="choice-image">`;
+    document.getElementById('machine-choice').innerHTML = `<img src="imagens/${machineChoice}.png" alt="${machineChoice}" class="choice-image">`;
+
+    if (playerMove === machineMove) {
+        resultMessage = 'Empate!';
+        document.getElementById('resultImage').src = 'imagens/Empate.png';
+    } else if ((playerMove === 1 && machineMove === 3) ||
+               (playerMove === 2 && machineMove === 1) ||
+               (playerMove === 3 && machineMove === 2)) {
+        resultMessage = 'Você ganhou!';
+        document.getElementById('resultImage').src = 'imagens/VoceGanhou.png';
+    } else {
+        resultMessage = 'Você perdeu!';
+        document.getElementById('resultImage').src = 'imagens/VocePerdeu.png';
+    }
+
+    document.getElementById('resultText').innerText = resultMessage;
+    document.querySelector('.result').classList.add('show');
 }
